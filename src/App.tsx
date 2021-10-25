3a44466853f77f8afe997adad281fafe1f95456e
tomoya_edzuka <80945931+t-edzuka@users.noreply.github.com>
2021-10-25 20:58:21 +0900

    [update] UI update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CustomWebcam from "./components/Camera/CustomWebcam";
+import CustomAppBar from "./components/AppBar/AppBar";
 
 const App = () => {
   return (
     <>
+      <CustomAppBar />
       <CustomWebcam />
     </>
   );
